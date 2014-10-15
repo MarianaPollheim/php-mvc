3538c2a0a71bc27e111d881ba98c660a568a7468
Mariana <github@marianapollheim.com.br>
2014-10-15 20:24:05 -0300

    aula31 - pequena melhoria no código do metodo getSong

--- application/models/songsmodel.php
+++ application/models/songsmodel.php
@@ -35,13 +35,18 @@ class SongsModel
     {
         $sql = "SELECT * FROM song WHERE id = $song_id";
         $query = $this->db->prepare($sql);
-        $query->execute();
+        $query->execute(array(':song_id'=> $song_id));
 
+        /*echo "<pre>  [DEBUG]: ";
+        var_dump ($query->fetch());
+        echo "</pre>";
+        die();*/
+        
         // fetchAll() is the PDO method that gets all result rows, here in object-style because we defined this in
         // libs/controller.php! If you prefer to get an associative array as the result, then do
         // $query->fetchAll(PDO::FETCH_ASSOC); or change libs/controller.php's PDO options to
         // $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ...
-        return $query->fetchAll();
+        return $query->fetch();
     }
 
     /**
